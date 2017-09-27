b4d80ef088f88c07c2fb57a2aca183600f76e8ca
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-09-27 12:46:16 +0100

    Replace part done.

--- SingleCharacterEdit.java
+++ SingleCharacterEdit.java
@@ -2,6 +2,43 @@ public class SingleCharacterEdit {
 
     public static String singleCharacterEditWillTurnAIntoB(String a, String b) {
         // TODO: your code goes here, replacing the next line with your actual code
+    	int aLength = a.length();
+    	int bLength = b.length();
+    	
+    	//Replace part
+    	if(aLength == bLength) {
+    		for (int i = 0; i < aLength; i++) {
+    			if(a.charAt(i) != b.charAt(i)){
+    				return "replace," + i + "," + b.charAt(i);
+    			}
+    		}
+    	}
+    	
+    	//Insert Part
+    	if(aLength - bLength == -1) {
+    		for (int i = 0; i < aLength; i++) {
+    			if (a.charAt(i) != b.charAt(i)) {
+    				return "insert," + i + "," + b.charAt(i);
+    			}
+    		}
+    		
+    		
+    		
+    		
+    	}
+    	
+    	
+    	//Remove Part
+    	if(aLength - bLength == 1) {
+    		
+    		
+    		
+    		
+    		
+    		
+    	}
+    	
+    	
         return null; // this is only here to make it compile
     }
 
