0e029fac915c41c74bdcda884e8c5fd75b032267
Dimitris Papatheodoulou <dimitrispapatheodoulou@gmail.com>
2017-10-02 12:24:14 +0100

    Methods setPixel, drawHorizontalLine and drawVerticalLine are implemented.

--- ScreenAsBytes.java
+++ ScreenAsBytes.java
@@ -1,15 +1,27 @@
 public class ScreenAsBytes {
 
     public static void setPixel(byte[] screen, int width, int x, int y) {
-        // TODO complete this code 
+    	
+    	int height = screen.length / (width / 8);
+    	int bitPosition = 7- x%8;  	
+    	
+    	screen[(y*2) + (x%width/8)] |= (byte) Math.pow(2, bitPosition);
+
+    	
     }
 
     public static void drawHorizontalLine(byte[] screen, int width, int startX, int endX, int y) {
-        // TODO complete this code
+    	
+    	for (int i=startX; i <= endX; i++) {
+    		setPixel(screen, width, i, y);
+    	}
     }
     
     public static void drawVerticalLine(byte[] screen, int width, int x, int startY, int endY) {
-        // TODO complete this code
+    	
+    	for (int i=startY; i <= endY; i++) {
+    		setPixel(screen, width, x, i);
+    	}
     }
 
 
